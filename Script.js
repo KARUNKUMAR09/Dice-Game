fc7d21c94962814c84dd06638b0eb896f386c315
KARUNKUMAR09 <kothapallikarunkumar@gmail.com>
2025-07-15 09:34:23 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,31 @@
+function randomImage(){
+    const img={
+        1: "../images/dice1.png",
+        2: "../images/dice2.png",
+        3: "../images/dice3.png",
+        4: "../images/dice4.png",
+        5: "../images/dice5.png",
+        6: "../images/dice6.png"
+    };
+    const randomimage1=Math.floor(Math.random()*6)+1;
+    const randomimage2=Math.floor(Math.random()*6)+1;
+    const img1=document.getElementById("imag1");
+     const img2=document.getElementById("imag2");
+     img1.setAttribute("src",img[randomimage1]);
+     img2.setAttribute("src",img[randomimage2]);
+     if(randomimage1>randomimage2){
+        document.querySelector("h1").innerHTML="player 1 wins!";
+     }
+     else if(randomimage1<randomimage2){
+        document.querySelector("h1").innerHTML="player 2 wins!";
+     }
+     else{
+        document.querySelector("h1").innerHTML="Draw!";
+     }
+
+
+
+    
+}
+functionrandomImage()
+
